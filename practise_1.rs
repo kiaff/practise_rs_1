27ee701e71885991f1b46597e041b50eb797f33e
kiaff <kiaff450@gmail.com>
2026-03-23 13:44:59 +0000

    Update practise_1.rs

--- practise_1.rs
+++ practise_1.rs
@@ -18,7 +18,23 @@ fn main(){
 }
 
 _______________________________________
-
+//learning another enum code example with value 
+enum DataSet{
+    Data1(String) ,
+    Data2(i32) ,
+}
+fn main(){
+    let value_1 : DataSet = DataSet::Data1(String::from("Hello Kiaff")) ;
+    let value_2 : DataSet = DataSet::Data2(22 as i32) ;
+    match value_1{
+        DataSet::Data1(value) => println!("The first data is a String :{:?}" , value) ,
+        DataSet::Data2(value) => println!("The second data is a int :{:?}" , value) ,
+    }
+    match value_2{
+        DataSet::Data1(value) => println!("The first data is a string is {:?}" , value) ,
+        DataSet::Data2(value) => println!("The second data is a 32 bit intiger is {:?}" , value) ,
+    }
+}
 
 
 
